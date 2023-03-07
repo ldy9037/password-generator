2ab628c4337d1c12348035aa8bfa63e938b44d06
emadam <ldy9037@naver.com>
2023-03-07 19:55:51 +0900

    test: Add test to set min and max value of character type

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -11,11 +11,9 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 class TestPasswordGenerator(unittest.TestCase):
 
     def setUp(self) -> None:
-        pass
+        self.generator = PasswordGenerator()
 
-    def test_generator_contains_common_character_type(self):
-        generator = PasswordGenerator()
-
+    def test_generator_contains_common_character_type(self) -> None:
         common_types = [
             ('uppercase', list(string.ascii_uppercase)),
             ('lowercase', list(string.ascii_lowercase)),
@@ -24,7 +22,23 @@ class TestPasswordGenerator(unittest.TestCase):
         ]
 
         for name, candidate in common_types:
-            self.assertListEqual(generator.types[name].candidate, candidate)
+            self.assertListEqual(
+                self.generator.types[name].candidate, candidate)
+
+    def test_set_min_and_max_of_character_type_(self) -> None:
+        common_types = [
+            ('uppercase', 0, 16),
+            ('lowercase', 1, 12),
+            ('digits', 2, 6),
+            ('special', 5, 5)
+        ]
+
+        for name, min, max in common_types:
+            self.generator.types[name].min = min
+            self.generator.types[name].max = max
+
+            self.assertEqual(self.generator.types[name].min, min)
+            self.assertEqual(self.generator.types[name].max, max)
 
 
 if __name__ == '__main__':
